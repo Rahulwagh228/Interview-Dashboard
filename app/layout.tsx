7731a4d29317705a7afb26e28557ddd220956c32
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2025-10-15 02:32:33 +0530

    implemented the font montSerrate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={montserrat.className}>
         {children}
         <Toaster />
       </body>
