b1c68e9520a3ed347d86755e6eba2c4e34bd9d5e
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2025-10-15 13:42:12 +0530

    inserted DancingScript and Shadow Into Light font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
+  display: "swap",
+});
+
+// Local font declarations
+const shadowsIntoLight = localFont({
+  src: "../public/fonts/ShadowsIntoLight-Regular.ttf",
+  variable: "--font-shadows-into-light",
+  display: "swap",
+});
+
+const dancingScript = localFont({
+  src: "../public/fonts/DancingScript-VariableFont_wght.ttf",
+  variable: "--font-dancing-script",
   display: "swap",
 });
 
@@ -14,7 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body 
+        className={`${montserrat.className} ${shadowsIntoLight.variable} ${dancingScript.variable}`}
+      >
         {children}
         <Toaster />
       </body>
